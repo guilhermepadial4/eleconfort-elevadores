73d4224a9780e6fce0f202bb181db8bdfe5d0777
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-29 13:52:52 -0300

    add: creation divider

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,57 +4,60 @@ import '../../styles/layout/content.scss';
 
 export function Content() {
   return (
-    <main className="containerContent">
-      <div className="contentLeft">
-        <h2 className="title">
-          VOCÊ CONHECE SEU ELEVADOR?
-          <span>ENTENDA COMO ELE FUNCIONA.</span>
-        </h2>
+    <>
+      <main className="containerContent">
+        <div className="contentLeft">
+          <h2 className="title">
+            VOCÊ CONHECE SEU ELEVADOR?
+            <span>ENTENDA COMO ELE FUNCIONA.</span>
+          </h2>
 
-        <img src={ElevatorOperation} alt="Funcionamento Elevador" />
-      </div>
+          <img src={ElevatorOperation} alt="Funcionamento Elevador" />
+        </div>
 
-      <div className="servicesList">
-        <h3>Serviços Prestados</h3>
-        <ul>
-          <li>
-            <span>Máquina de tração</span>
-            <p>Substituição do cabo de aço</p>
-          </li>
-          <li>
-            <span>Limitador de velocidade</span>
-            <p>Manutenção do regulador de velocidade</p>
-          </li>
-          <li>
-            <span>Topo da cabine</span>
-            <p>Limpeza e pintura do topo da cabine</p>
-          </li>
-          <li>
-            <span>Botoeira de cabine</span>
-            <p>Modernização das botoeiras</p>
-          </li>
-          <li>
-            <span>Botoeira do pavimento</span>
-            <p>Modernização das botoeiras</p>
-          </li>
-          <li>
-            <span>Quadro de comando</span>
-            <p>Modernização do quadro</p>
-          </li>
-          <li>
-            <span>Caixa de inspeção</span>
-            <p>Modernização da caixa</p>
-          </li>
-          <li>
-            <span>Operador de porta</span>
-            <p>Manutenção do operador</p>
-          </li>
-          <li>
-            <span>Display</span>
-            <p>Substituição e Modernização do display</p>
-          </li>
-        </ul>
-      </div>
-    </main>
+        <div className="servicesList">
+          <h3>Serviços Prestados</h3>
+          <ul>
+            <li>
+              <span>Máquina de tração</span>
+              <p>Substituição do cabo de aço</p>
+            </li>
+            <li>
+              <span>Limitador de velocidade</span>
+              <p>Manutenção do regulador de velocidade</p>
+            </li>
+            <li>
+              <span>Topo da cabine</span>
+              <p>Limpeza e pintura do topo da cabine</p>
+            </li>
+            <li>
+              <span>Botoeira de cabine</span>
+              <p>Modernização das botoeiras</p>
+            </li>
+            <li>
+              <span>Botoeira do pavimento</span>
+              <p>Modernização das botoeiras</p>
+            </li>
+            <li>
+              <span>Quadro de comando</span>
+              <p>Modernização do quadro</p>
+            </li>
+            <li>
+              <span>Caixa de inspeção</span>
+              <p>Modernização da caixa</p>
+            </li>
+            <li>
+              <span>Operador de porta</span>
+              <p>Manutenção do operador</p>
+            </li>
+            <li>
+              <span>Display</span>
+              <p>Substituição e Modernização do display</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <div className="divider"></div>
+    </>
   );
 }
