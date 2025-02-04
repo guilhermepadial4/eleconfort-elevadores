1cc90886b1d556d98d56042fd6f5a6661f620e4a
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-04 12:03:05 -0300

    update: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,17 @@ import '../../styles/layout/header.scss';
 
 export function Header() {
   return (
-    <>
-      <header className="containerHeader">
+    <header className="containerHeader">
+      <Link to="/">
         <img src={Logo} alt="Logo da empresa Eleconfort" />
+      </Link>
 
-        <div className="containerLinks">
-          <Link to="/">Inicio</Link>
-          <Link to="/sobre">Sobre nós</Link>
-          <Link to="/servicos">Serviços</Link>
-          <Link to="/contato">Contato</Link>
-        </div>
-      </header>
-    </>
+      <div className="containerLinks">
+        <Link to="/">Inicio</Link>
+        <Link to="/sobre">Sobre nós</Link>
+        <Link to="/servicos">Serviços</Link>
+        <Link to="/contato">Contato</Link>
+      </div>
+    </header>
   );
 }
