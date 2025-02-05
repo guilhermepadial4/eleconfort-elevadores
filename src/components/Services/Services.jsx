62fcf5a7ad09d4ca34e3dcb7db3fd811021ccb4a
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-05 14:45:59 -0300

    update: add responsive in services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -34,6 +34,20 @@ export function ServicesPictures() {
     dots: true,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
