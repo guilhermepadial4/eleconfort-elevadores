b3b032992d79872548ffb483db7a9ea001b95c32
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-05 11:42:57 -0300

    update: responsive in home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { FiMenu, FiX } from 'react-icons/fi';
 
 import Logo from '../../assets/logo.png';
 
 import '../../styles/layout/header.scss';
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="containerHeader">
       <Link to="/">
@@ -12,10 +17,29 @@ export function Header() {
       </Link>
 
       <div className="containerLinks">
-        <Link to="/">Inicio</Link>
+        <Link to="/">Início</Link>
         <Link to="/sobre">Sobre nós</Link>
         <Link to="/servicos">Serviços</Link>
         <Link to="/contato">Contato</Link>
+      </div>
+
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
+
+      <div className={`mobile-menu ${menuOpen ? 'active' : ''}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>
+          Início
+        </Link>
+        <Link to="/sobre" onClick={() => setMenuOpen(false)}>
+          Sobre nós
+        </Link>
+        <Link to="/servicos" onClick={() => setMenuOpen(false)}>
+          Serviços
+        </Link>
+        <Link to="/contato" onClick={() => setMenuOpen(false)}>
+          Contato
+        </Link>
       </div>
     </header>
   );
