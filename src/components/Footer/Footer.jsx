d983b3b04f35cd71d3a23bfc55f0f5821e48f54f
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-06 11:16:56 -0300

    add: links in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,30 +23,41 @@ export function Footer() {
         <div className="footerAddres">
           <h3>Onde estamos</h3>
           <p>
-            <MdLocationOn size={20} /> Praça Haroldo Daltro, 394 - São Paulo, SP
+            <a href="https://www.google.com/maps?q=Praça+Haroldo+Daltro,+394+-+São+Paulo,+SP" target="_blank">
+              <MdLocationOn size={20} /> Praça Haroldo Daltro, 394 - São Paulo, SP
+            </a>
           </p>
         </div>
 
         <div className="footerContact">
           <h3>Contatos</h3>
           <p>
-            <MdPhone size={20} /> (11) 1234-5678
+            <a href="tel:+551120517645" target="_blank">
+              <MdPhone size={20} /> (11) 2051-7645
+            </a>
           </p>
           <p>
-            <FaWhatsapp size={20} /> (11) 98765-4321
+            <a
+              href="https://api.whatsapp.com/send?1=pt_BR&text=Ol%C3%A1,%20Tudo%20bem?%20Gostaria%20de%20Saber%20Mais%20Informa%C3%A7%C3%B5es.&phone=5511991239476"
+              target="_blank"
+            >
+              <FaWhatsapp size={20} /> (11) 99123-9476
+            </a>
           </p>
           <p>
-            <MdEmail size={20} /> comercial@eleconfortelevadores.com.br
+            <a href="mailto:comercial@eleconfortelevadores.com.br" target="_blank">
+              <MdEmail size={20} /> comercial@eleconfortelevadores.com.br
+            </a>
           </p>
         </div>
 
         <div className="footerMidia">
           <h3>Redes socias</h3>
           <div className="socialIcons">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/eleconfortelevadores" target="_blank" rel="noopener noreferrer">
               <FaFacebook size={24} /> Facebook
             </a>
-            <a className="iconInstagram" href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <a className="iconInstagram" href="https://www.instagram.com/eleconfortelevadores/" target="_blank" rel="noopener noreferrer">
               <FaInstagram size={24} /> Instagram
             </a>
           </div>
