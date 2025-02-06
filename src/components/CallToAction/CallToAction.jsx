46b780059a5ac3858f6829b3aa3297992d110a38
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-06 11:18:52 -0300

    add: links in calltoaction

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -4,7 +4,12 @@ export function CallToAction() {
   return (
     <section className="call-to-action">
       <h2>Deseja agendar uma conversa?</h2>
-      <button className="cta-button">Fale Conosco</button>
+      <a
+        href="https://api.whatsapp.com/send?1=pt_BR&text=Ol%C3%A1,%20Tudo%20bem?%20Gostaria%20de%20Saber%20Mais%20Informa%C3%A7%C3%B5es.&phone=5511991239476"
+        target="_blank"
+      >
+        <button className="cta-button">Fale Conosco</button>
+      </a>
     </section>
   );
 }
