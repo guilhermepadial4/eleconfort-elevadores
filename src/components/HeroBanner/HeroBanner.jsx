2e91804889be9d27efc540a75d47d4714b21803b
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-20 11:19:55 -0300

    add: animation in banner

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
+
 import Buttons from '../../assets/butons.png';
+import Build from '../../assets/build-01.png';
+import Build2 from '../../assets/build-02.png';
+import Build3 from '../../assets/build-03.png';
+
 import '../../styles/layout/herobanner.scss';
 
 export const HeroBanner = () => {
+  const images = [Buttons, Build, Build2, Build3];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
-    <section className="hero-banner" style={{ backgroundImage: `url(${Buttons})` }}>
+    <section className="hero-banner" style={{ backgroundImage: `url(${images[currentImageIndex]})` }}>
       <div className="hero-banner__overlay">
         <div className="hero-banner__content">
           <h1>
